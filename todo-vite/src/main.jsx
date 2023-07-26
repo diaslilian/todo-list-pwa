05d248294914e51796c7441bc96110f19d0735d0
Lilian Fernandes <lilian.dias25@gmail.com>
2023-07-26 14:45:22 -0300

    feat: config vitepwa

--- todo-vite/src/main.jsx
+++ todo-vite/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { registerSW } from "virtual:pwa-register";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// add this to prompt for a refresh
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location) && !/lvh.me/.test(window.location)) {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      Toastify({
+        text: `<h4 style='display: inline'>An update is available!</h4>
+               <br><br>
+               <a class='do-sw-update'>Click to update and reload</a>  `,
+        escapeMarkup: false,
+        gravity: "bottom",
+        onClick() {
+          updateSW(true);
+        },
+      }).showToast();
+    },
+  });
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- todo-vite/vite.config.js
+++ todo-vite/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        sourcemap: true,
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith("/"),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
